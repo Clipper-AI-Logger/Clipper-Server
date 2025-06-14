983a8284bdb03ec85aa80136522f924449f21bd3
aestera <aest3ra@gmail.com>
2025-06-14 16:39:11 +0900

    [frontend] fix: bug fixed

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -18,10 +18,11 @@ const jsonApi = axios.create({
 });
 
 const createTusUpload = (file, metadata) => {
-    
     return new Promise((resolve, reject) => {
         const upload = new tus.Upload(file, {
-            endpoint: `${baseURL}/edit/upload`,
+            endpoint: import.meta.env.VITE_DEV === '0' 
+                ? 'http://127.0.0.1:8000/edit/upload'
+                : 'https://clippergpt.com/api/edit/upload',
             chunkSize: 5 * 1024 * 1024,
             retryDelays: [0, 1000, 3000],
             metadata: {
